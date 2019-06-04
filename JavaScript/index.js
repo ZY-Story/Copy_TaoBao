48ced57ff4e63f1966969ccf6c9fde5bf0b539b7
v_yazzzhang(张亚) <v_yazzzhang@tencent.com>
2019-06-04 18:03:44 +0800

    'js修改'

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,30 @@
-window.onload = function(){
-    var headerIndex = document.getElementsByClassName('headerIndex')
-    var left = document.getElementsByClassName('left')
+// window.onload = function(){
+//     // var headerIndex = document.getElementsByClassName('headerIndex')
+//     // var right = document.getElementByClassName('right')
+//     // console.log(this.right)
+//     alert(0)
+// }
+// (function(){
+//     var headerIndex = document.getElementsByClassName('headerIndex')
+//     var right = document.getElementByClassName('right')
+//     console.log(right)
+//     console.log(headerIndex)
+//     alert(123)
+// })
 
+
+
+// 淘宝网首页
+function getIndex(){
+    var tbIndex = document.getElementById('tbIndex')
+    console.log(tbIndex.innerHTML)     
+}
+// 我的淘宝
+function getMyTb(){
+    var tbOther = document.getElementById('tbOther')
+    tbOther.style.display ='block'
+}
+function leaveMyTb(){
+    var tbOther = document.getElementById('tbOther')
+    tbOther.style.display ='none'
 }
